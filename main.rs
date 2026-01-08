600619444d5c65cba331d76844bfcfe8cb64f82d
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-01-08 23:37:40 +0100

    Update Rocket version handling in main.rs

--- main.rs
+++ main.rs
@@ -1,9 +1,10 @@
 #[macro_use] extern crate rocket;
 
 #[get("/?<name>")]
+use rocket::config::Config;
 fn index(name: Option<&str>) -> String {
-  let version = rocket::Config::release_profile();
-  format!("Hello, {}!<br>(Rocket v{})", name.unwrap_or("world"), version)
+  let version = "0.5.0";
+  format!("Hello, {}!<br>(Rocket v{})", name.unwrap_or("world"), Config::release())
 }
 
 #[launch]
